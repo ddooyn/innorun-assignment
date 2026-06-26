d5a22db66158157bc0ec4512958672d6ed606778
Sooyoung Yoon <ayoooo240@gmail.com>
2026-06-26 11:48:35 +0900

    feat: Student 구현

--- src/main/java/org/practice/Main.java
+++ src/main/java/org/practice/Main.java
@@ -2,6 +2,7 @@ package org.practice;
 
 public class Main {
     public static void main(String[] args) {
-//        TODO
+        Student student = new Student(1, "김하나", 87);
+        System.out.println(student.toString());
     }
 }
--- src/main/java/org/practice/Student.java
+++ src/main/java/org/practice/Student.java
@@ -1,5 +1,45 @@
 package org.practice;
 
 public class Student {
-//    TODO
+    private final int id;
+    private final String name;
+    private int score;
+
+    public Student(int id, String name, int score) {
+        this.id = id;
+        this.name = name;
+
+        if (score < 0 || score > 100) {
+            throw new IllegalArgumentException("점수는 0부터 100 사이의 정수여야 합니다.");
+        }
+        this.score = score;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void updateScore(int score) {
+        if (score < 0 || score > 100) {
+            throw new IllegalArgumentException("점수는 0부터 100 사이의 정수여야 합니다.");
+        }
+        this.score = score;
+    }
+
+    public PassStatus getPassStatus() {
+        return score >= 60 ? PassStatus.PASSED : PassStatus.FAILED;
+    }
+
+    @Override
+    public String toString() {
+        return "Student " + id + " / " + name + " / " + score + "점 -> " + getPassStatus().getLabel();
+    }
 }
