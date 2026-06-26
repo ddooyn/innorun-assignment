d35b3d884d4d88152af086117776f957f2f28d5a
Sooyoung Yoon <ayoooo240@gmail.com>
2026-06-26 12:05:20 +0900

    feat: Classroom 구현

--- src/main/java/org/practice/Classroom.java
+++ src/main/java/org/practice/Classroom.java
@@ -1,5 +1,44 @@
 package org.practice;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
 public class Classroom {
-//    TODO
+    private final List<Student> students;
+
+    public Classroom() {
+        this.students = new ArrayList<>();
+    }
+
+    public List<Student> getStudents() {
+        return List.copyOf(students);
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public Optional<Student> findById(int id) {
+        return students.stream().filter(s -> s.getId() == id).findFirst();
+    }
+
+    public List<Student> getPassedStudents() {
+        return students.stream().filter(s -> s.getPassStatus() == PassStatus.PASSED).toList();
+    }
+
+    public Optional<Student> findTopStudent() {
+        if (students.isEmpty()) return Optional.empty();
+
+        Student top = students.getFirst();
+        for (Student s : students) {
+            if (s.getScore() > top.getScore()) top = s;
+        }
+        return Optional.of(top);
+    }
+
+    public List<Student> searchByName(String name) {
+        return students.stream().filter(s -> s.getName().equals(name)).toList();
+    }
 }
