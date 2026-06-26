eb798440adc6a770261c1ce28206afa2d8d5132b
Sooyoung Yoon <ayoooo240@gmail.com>
2026-06-26 12:17:15 +0900

    feat: 초기 학생 목록 설정

--- src/main/java/org/practice/Main.java
+++ src/main/java/org/practice/Main.java
@@ -1,8 +1,29 @@
 package org.practice;
 
+import java.util.Scanner;
+
 public class Main {
+    private final Scanner scanner;
+
+    public Main() {
+        this.scanner = new Scanner(System.in);
+    }
+
     public static void main(String[] args) {
-        Student student = new Student(1, "김하나", 87);
-        System.out.println(student.toString());
+        new Main().run();
+    }
+
+    public void run() {
+        try {
+            Classroom classroom = new Classroom();
+
+            classroom.addStudent(new Student(1, "김하나", 87));
+            classroom.addStudent(new Student(2, "이도윤", 92));
+            classroom.addStudent(new Student(3, "박서준", 58));
+
+//            TODO
+        } finally {
+            scanner.close();
+        }
     }
 }
