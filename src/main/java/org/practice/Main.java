46fb21a62a1177065f581fe80808896f8bcc256a
Sooyoung Yoon <ayoooo240@gmail.com>
2026-06-26 13:30:35 +0900

    feat: 사용자 입력 처리

--- src/main/java/org/practice/Main.java
+++ src/main/java/org/practice/Main.java
@@ -23,15 +23,25 @@ public class Main {
             classroom.addStudent(new Student(3, "박서준", 58));
             classroom.addStudent(new Student(4, "정다은", 75));
 
-            updateStudentScoreById(classroom, 2, 96);
-            printPassedStudents(classroom);
-            printTopScoreStudent(classroom);
-
             printAllStudents(classroom);
-            updateStudentScoreById(classroom, 99, 100);
-            printStudentByNameKeyword(classroom, "김");
-            printStudentByNameKeyword(classroom, "최");
-            updateStudentScoreById(classroom, 4, 200);
+
+            while (true) {
+                printMenu();
+                System.out.print("\uD83D\uDC49 ");
+                String command = scanner.nextLine().toLowerCase().trim();
+
+                switch (command) {
+                    case "a" -> printAllStudents(classroom);
+                    case "t" -> printTopScoreStudent(classroom);
+                    case "p" -> printPassedStudents(classroom);
+                    case "u" -> updateStudentScoreById(classroom);
+                    case "n" -> printStudentByNameKeyword(classroom);
+                    case "q" -> {
+                        return;
+                    }
+                    default -> System.out.println("⚠️ 존재하지 않는 명령어입니다.");
+                }
+            }
         } catch(IllegalArgumentException e) {
             System.out.println("❌ " + e.getMessage());
         } finally {
@@ -39,13 +49,33 @@ public class Main {
         }
     }
 
-    private void updateStudentScoreById(Classroom classroom, int id, int score) {
+    private void printMenu() {
+        System.out.println("""
+            =================
+            [a] 전체 학생 조회
+            [t] 최고점 학생 조회
+            [p] 합격자 조회
+            [u] 학생 점수 수정
+            [n] 이름 검색
+            [q] 종료
+            """);
+    }
+
+    private void updateStudentScoreById(Classroom classroom) {
+        System.out.print("점수를 수정할 학생 ID: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
         Optional<Student> target = classroom.findById(id);
 
         if (target.isEmpty()) {
             System.out.println("⚠️ 조건에 맞는 학생을 찾을 수 없습니다. (id: " + id + ")");
             return;
         }
+
+        System.out.print("점수: ");
+        int score = scanner.nextInt();
+        scanner.nextLine();
 
         target.get().updateScore(score);
         System.out.println("✅ 점수를 수정했습니다.");
@@ -70,7 +100,9 @@ public class Main {
         System.out.println("최고점: " + top.get());
     }
 
-    private void printStudentByNameKeyword(Classroom classroom, String keyword) {
+    private void printStudentByNameKeyword(Classroom classroom) {
+        System.out.print("학생 이름: ");
+        String keyword = scanner.nextLine().trim();
         List<Student> targets = classroom.searchByName(keyword);
 
         if (targets.isEmpty()) {
